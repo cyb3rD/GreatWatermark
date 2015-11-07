440045c1fe41be6fe833c99f0f0eb1e9a9522152
chernobelenkiy <chernobelenkiy@gmail.com>
2015-11-08 00:13:37 +0300

    add getimage

--- app/php/getimage.php
+++ app/php/getimage.php
@@ -1,10 +1,40 @@
 <?php
+use WideImage\WideImage;
 
-// Файл будет скачиваться с определенным нами названием
+require_once 'plugins/autoload.php';
 
-$result = '../images/#';
+$result = '../images/result.jpg';
 
-header('Content-type: application/jpeg');
-header('Content-Disposition: attachment; filename="#"');
+$opacity = $_POST['opacity'];
+$deltaX = $_POST['deltaX'];
+$deltaY = $_POST['deltaY'];
+$tiling = $_POST['tiling'];
+$image = WideImage::load($_POST['image']);
+$watermark = WideImage::load($_POST['watermark']);
+$sizeImg = getimagesize($_POST['image']);
+$sizeWt = getimagesize($_POST['watermark']);
+$marginX = $_POST['marginX'];
+$marginY = $_POST['marginY'];
+$lengthX = $marginY;
+$lengthY = $marginX;
+$i = 0;
+$j = 0;
 
-readfile($result);
+if($tiling) {
+    $new = $image->merge($watermark, 'left + 0', 'top + 0', 0);
+    while ($lengthY < $sizeImg[1]) {
+        $lengthX = $marginX;
+        while ($lengthX < $sizeImg[0]) {
+            $new = $new->merge($watermark, 'left  + ' . $lengthX, 'top  +' . $lengthY, $opacity);
+            $lengthX = $lengthX + $sizeWt[0] + $marginX;
+        }
+        $lengthY = $lengthY + $sizeWt[1] + $marginY;
+    }
+    $new -> saveToFile($result);
+} else {
+    $new = $image->merge($watermark, 'left + ' . $deltaX, 'top+' . $deltaY, $opacity);
+    $new -> saveToFile($result);
+}
+
+
+exit;
