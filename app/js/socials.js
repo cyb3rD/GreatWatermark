4fdd89a1474f26047df3a250c329ea5deacf1b7b
ivarden <varden.adams@gmail.com>
2015-11-07 10:18:03 -0800

    add socials icons function

---
+++ app/js/socials.js
@@ -0,0 +1,27 @@
+//  twitter
+! function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    p = /^http:/.test(d.location) ? 'http' : 'https';
+  if (!d.getElementById(id)) {
+    js = d.createElement(s);
+    js.id = id;
+    js.src = p + '://platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+}(document, 'script', 'twitter-wjs');
+
+//  VK
+document.write(VK.Share.button(false, {
+  type: "custom",
+  text: "<img src=\"http://vk.com/images/share_32_2x.png\" width=\"32\" height=\"32\" style=\"position:absolute; left:99999px;\"/>"
+}));
+
+//  facebook
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
